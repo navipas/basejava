0e5e1d7e92f1bf0455cc55dae12ee43ff0e51538
navipas <navipas88@gmail.com>
2022-12-10 16:35:52 +0400

    Update ArrayStorage.java
    Добавлено поле size, изменены методы с учетом нового поля

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,46 +5,39 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-            }
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
-        }
+        storage[size] = r;
+        size = size + 1;
     }
 
     Resume get(String uuid) {
-
-        for (int i = 0; i < storage.length; i++) {
-            Resume resume = storage[i];
-            if (resume != null && resume.uuid.equals(uuid)) {
-                return resume;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            Resume resume = storage[i];
-            if (resume.uuid.equals(uuid)) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 storage[i] = null;
-                for (int j = i; j < storage.length - 1; j++) {
+                size = size - 1;
+                for (int j = i; j < size; j++) {
                     storage[j] = storage[j + 1];
                 }
                 break;
             }
-
         }
     }
 
@@ -52,22 +45,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int count = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                count++;
-            }
-        }
-        return Arrays.copyOf(storage, count);
+         return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int count = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                count++;
-            }
-        }
-        return count;
+        return size;
     }
 }
